1bd7d08b82ca9991f65d36d401cd638a8294c69a
Medea Boudreaux <mcbdrx@gmail.com>
2019-05-10 17:07:17 -0500

    finished user-signup

--- main.py
+++ main.py
@@ -6,14 +6,22 @@ app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RU'
 
 
 def is_email(string):
-    atsign_index = string.find('@')
-    atsign_present = atsign_index >= 0
-    if not atsign_present:
-        return False
+    if string:
+        bad_chars = "!#$ %^&*()+|}{:~-=[]"    
+        for char in bad_chars:
+            if char in string:
+                return False  
+    
+        atsign_index = string.find('@')
+        atsign_present = atsign_index >= 0
+        if not atsign_present:
+            return False
+        else:
+            domain_dot_index = string.find('.', atsign_index)
+            domain_dot_present = domain_dot_index >= 0
+            return domain_dot_present
     else:
-        domain_dot_index = string.find('.', atsign_index)
-        domain_dot_present = domain_dot_index >= 0
-        return domain_dot_present
+        return False
 
 def is_valid_password(string):
     if len(string) > 20 or len(string) < 3:
@@ -22,8 +30,7 @@ def is_valid_password(string):
     for char in bad_chars:
         if char in string:
             return False  
-    else:
-        return True          
+    return True          
 
 def is_valid_username(string):
     if len(string) > 20 or len(string) < 3:
@@ -34,8 +41,10 @@ def is_valid_username(string):
 
 @app.route("/welcome", methods=["GET"])
 def welcome():
-    user = session["username"]
-    return render_template("welcome.html", user=user)
+    username = session["username"]
+    return render_template("welcome.html", username=username)
+    if "username" not in session:
+        return redirect("/")    
 
 @app.route("/", methods=["GET", "POST"])
 def signup():
@@ -44,24 +53,32 @@ def signup():
         password = request.form["password"]
         verify = request.form["verify"]
         username = request.form["username"]
+        error_counter = 0
 
-        if not email and not password and not verify and not username:
-            flash("You must enter a username and password.", "error")    
         if email and not is_email(email):
-            flash("You must enter a valid email.", "email_error")   
+            flash("You must enter a valid email.", "email_error") 
+            error_counter += 1  
         if password != verify:
-            flash("Passwords do not match.", "password_error")    
+            flash("Passwords do not match.", "password_error")
+            error_counter += 1    
         if not is_valid_password(password):
             flash("You must enter a valid password. Must be 3-20 characters long and cannot contain spaces.", "password_error")
+            error_counter += 1  
         if email and not is_valid_username(username):
             flash("You must enter a valid username. Must be 3-20 characters long.", "username_error")
-        if not email and not is_valid_username(username):
-            flash("You must enter a valid username. Must be 3-20 characters long.", "username_error")
-        else:
-            user = session["username"]
-            return redirect("welcome.html", user=user)
+            error_counter += 1  
+        # if not email and not is_valid_username(username):
+        #     flash("You must enter a valid username. Must be 3-20 characters long.", "username_error")
+        #     error_counter += 1  
+        if error_counter != 0:
+            return render_template("signup.html", error_counter=error_counter)
 
-    return render_template("signup.html")
+        else:
+            session["username"] = username
+            return redirect("/welcome")
+        
+    else: 
+        return render_template("signup.html")
 
 
 app.run()
