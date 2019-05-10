90a6c204c142154ddd798deef92fcee8b5682fa9
Medea Boudreaux <mcbdrx@gmail.com>
2019-05-09 21:03:30 -0500

    finished error messages next to input fields & added welcome page without user name connected

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from flask import Flask, request, redirect, render_template, flash
+from flask import Flask, request, redirect, render_template, flash, session
+
 app = Flask(__name__)
 app.config["DEBUG"] = True
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RU'
+
 
 def is_email(string):
     atsign_index = string.find('@')
@@ -21,33 +24,44 @@ def is_valid_password(string):
             return False  
     else:
         return True          
-    
-# TODO fix error message
+
+def is_valid_username(string):
+    if len(string) > 20 or len(string) < 3:
+        return False
+    else:
+        return True    
+
+
+@app.route("/welcome", methods=["GET"])
+def welcome():
+    user = session["username"]
+    return render_template("welcome.html", user=user)
+
 @app.route("/", methods=["GET", "POST"])
 def signup():
     if request.method == "POST":
         email = request.form["email"]
         password = request.form["password"]
         verify = request.form["verify"]
+        username = request.form["username"]
 
-        if not is_email(email) or not email:
-            flash("You must enter a valid email.")
-            return redirect("/")
+        if not email and not password and not verify and not username:
+            flash("You must enter a username and password.", "error")    
+        if email and not is_email(email):
+            flash("You must enter a valid email.", "email_error")   
         if password != verify:
-            flash("Password verify did not match password.")
-            return redirect("/")
-        if not password or not verify:
-            flash("You must enter a password.")
-            return redirect("/")
+            flash("Passwords do not match.", "password_error")    
         if not is_valid_password(password):
-            flash("Your password must be 3-20 characters in length and must not contain any of these characters: '!@#$%^&*()+|}{:~-=[]'")
-            return redirect("/")
+            flash("You must enter a valid password. Must be 3-20 characters long and cannot contain spaces.", "password_error")
+        if email and not is_valid_username(username):
+            flash("You must enter a valid username. Must be 3-20 characters long.", "username_error")
+        if not email and not is_valid_username(username):
+            flash("You must enter a valid username. Must be 3-20 characters long.", "username_error")
         else:
-            return render_template("signup.html")
+            user = session["username"]
+            return redirect("welcome.html", user=user)
 
     return render_template("signup.html")
 
-
-app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RU'
 
 app.run()
